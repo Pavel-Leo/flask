746de481b418c34c05a9c1b8aec5852c95a06a08
Павел Леонтьев <fionit-2007@mail.ru>
2023-10-09 01:57:35 +0300

    feat: Добавлена форма для отправки сообщений. Внедрена возможность отправки сообщений через форму на главной странице без перезагрузки страницы. Используется AJAX-запрос в index html для отправки данных на сервер и отображения ответа без перезагрузки страницы.

--- flask_site.py
+++ flask_site.py
@@ -1,11 +1,57 @@
-from flask import Flask, render_template
+import os
+
+from flask import (
+    Flask,
+    flash,
+    get_flashed_messages,
+    render_template,
+    request,
+    url_for,
+    jsonify
+)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+MENU: list[dict[str, str]] = [
+    {'name': 'ГЛАВНАЯ', 'url': '/'},
+    {'name': 'О НАС', 'url': 'about'},
+    {'name': 'СЕРВИСЫ', 'url': 'services'},
+    {'name': 'КОНТАКТЫ', 'url': 'contact'},
+]
 
 
-@app.route('/')
+# from flask import jsonify
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        name = request.form['username']
+        phone = request.form['phone']
+        response_data = {'status': 'error', 'message': 'Ошибка при отправке формы'}
+
+        if len(name) > 0 and name.isalpha() and len(phone) > 10:
+            if (phone[:2] == '+7' and len(phone) == 12) or (
+                phone[0] == '8' and len(phone) == 11
+            ):
+                response_data = {
+                    'status': 'success',
+                    'message': 'Сообщение отправлено. Наш специалист свяжется с вами в ближайшее время.',
+                }
+            else:
+                response_data['message'] = 'Неверный формат номера телефона'
+        else:
+            response_data['message'] = (
+                'Не все поля заполнены верно. Поле "Имя" не может быть пустым и должно содержать только буквы, формат телефона 8 или +7'
+            )
+
+        return jsonify(response_data)
+
+    return render_template('index.html', title='Главная страница', menu=MENU)
+
+@app.route('/profile/<int:username>')
+def profile(username):
+    return f'Пользователь {username}'
 
 
 @app.route('/about')
