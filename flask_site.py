a1d880e9603e7d7db59cc4ef5b74b3cff9ba1429
Павел Леонтьев <fionit-2007@mail.ru>
2023-10-09 03:30:07 +0300

    feat: Добавлен лбработчик страницы 404. Реализована кастомная страница обработки 404. В style.css добавлены стили реализующую вывод стороннего gif изображения в 404.html с возможностью перейти на главную страницу.

--- flask_site.py
+++ flask_site.py
@@ -4,10 +4,10 @@ from flask import (
     Flask,
     flash,
     get_flashed_messages,
+    jsonify,
     render_template,
     request,
     url_for,
-    jsonify
 )
 
 app = Flask(__name__)
@@ -20,15 +20,19 @@ MENU: list[dict[str, str]] = [
     {'name': 'КОНТАКТЫ', 'url': 'contact'},
 ]
 
-
-# from flask import jsonify
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', title='Страница не найдена', menu=MENU)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         name = request.form['username']
         phone = request.form['phone']
-        response_data = {'status': 'error', 'message': 'Ошибка при отправке формы'}
+        response_data = {
+            'status': 'error',
+            'message': 'Ошибка при отправке формы',
+        }
 
         if len(name) > 0 and name.isalpha() and len(phone) > 10:
             if (phone[:2] == '+7' and len(phone) == 12) or (
@@ -36,18 +40,54 @@ def index():
             ):
                 response_data = {
                     'status': 'success',
-                    'message': 'Сообщение отправлено. Наш специалист свяжется с вами в ближайшее время.',
+                    'message': (
+                        'Сообщение отправлено. Наш специалист свяжется с вами'
+                        ' в ближайшее время.'
+                    ),
                 }
             else:
                 response_data['message'] = 'Неверный формат номера телефона'
         else:
             response_data['message'] = (
-                'Не все поля заполнены верно. Поле "Имя" не может быть пустым и должно содержать только буквы, формат телефона 8 или +7'
+                'Не все поля заполнены верно. Поле "Имя" не может быть пустым'
+                ' и должно содержать только буквы, формат телефона 8 или +7'
             )
-
         return jsonify(response_data)
-
     return render_template('index.html', title='Главная страница', menu=MENU)
+
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         name = request.form['username']
+#         phone = request.form['phone']
+#         if len(name) > 0 and name.isalpha() and len(phone) > 10:
+#             if (phone[:2] == '+7' and len(phone) == 12) or (
+#                 phone[0] == '8' and len(phone) == 11
+#             ):
+#                 flash(
+#                     (
+#                         'Сообщение отправлено. Наш специалист свяжется с вами'
+#                         ' в ближайшее время.'
+#                     ),
+#                     category='success',
+#                 )
+#             else:
+#                 flash('Неверный формат номера телефона', category='error')
+#         else:
+#             flash(
+#                 (
+#                     'Не все поля заполнены верно. Поле "Имя" не может быть'
+#                     ' пустым и должно содержать только буквы, формат телефона'
+#                     ' 8 или +7'
+#                 ),
+#                 category='error',
+#             )
+#         for k, v in request.form.items():
+#             print(v, end=', ')
+#     print(url_for('index'))
+#     return render_template('index.html', title='Главная страница', menu=MENU)
+
 
 @app.route('/profile/<int:username>')
 def profile(username):
